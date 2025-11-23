571c7d012293658051a67ae056845315410aea7e
PDDong-degug <goshufake10@gmail.com>
2025-11-23 23:47:46 +0700

    Add basic program to sum two integers
    Signed-off-by: PDDong-degug <goshufake10@gmail.com>

---
+++ tonghaiso.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b;
+    printf("nhap vao so a : ");
+    scanf("%d", &a);
+    printf("nhap vao so b : ");
+    scanf("%d", &b);
+    printf("tong cua a va b la : %d\n", a + b);
+    return 0;
+}
